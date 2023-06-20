ed7e2d35f6b9467a623b1779f465aff4f5a05e0a
rohit8450 <rohitrajthakur10@gmail.com>
2023-06-20 12:56:02 +0530

    BackendApi

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ const express = require('express');
 
 const router = express.Router();
 
+
+router.get('/', (req, res) => {
+
+    return res.json({ "Welcome": ` to my Backend Software for the Items ` });
+
+});
 router.use("/api", require('./items'));
 
 module.exports = router;
